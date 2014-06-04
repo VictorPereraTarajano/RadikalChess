719bf61e05cf3b98a4a9035bb5019926dae75830
Viiic <viic.laspalmas@gmail.com>
2014-06-04 22:42:19 +0100

    Create RadikalChessAction.java

---
+++ RadikalChessAction.java
@@ -0,0 +1,32 @@
+package aima.core.environment.radikalchess;
+
+import aima.core.util.datastructure.XYLocation;
+
+public class RadikalChessAction  {
+
+	private RadikalChessPiece piece;
+	private XYLocation locFinal;
+	private boolean coronation = false;
+	
+	public RadikalChessAction (XYLocation loc, RadikalChessPiece piece) {
+		this.locFinal = loc;
+		this.piece = piece;
+	}
+	
+	public RadikalChessAction (XYLocation locFinal, RadikalChessPiece pieceToChange, boolean coronation) {
+		this(locFinal, pieceToChange);
+		this.coronation = coronation;
+	}
+	
+	public XYLocation getXYLocation () {
+		return this.locFinal;
+	}
+	
+	public boolean isCoronationMove () {
+		return this.coronation;
+	}
+	
+	public RadikalChessPiece getPiece () {
+		return this.piece;
+	}
+}
